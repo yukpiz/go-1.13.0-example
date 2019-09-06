b942a203da60d4c43dbe7fa44f0f3c866e512f83
yukpiz <yukpiz@gmail.com>
2019-09-06 10:09:17 +0900

    up

---
+++ literal/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x1 := 0x111
+	fmt.Println(x1)
+
+	b1 := 0b111
+	fmt.Println(b1)
+
+	o1 := 0o660
+	fmt.Println(o1)
+
+	i := 1_000_000
+	fmt.Println(i)
+}
